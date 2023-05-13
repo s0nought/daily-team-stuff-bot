0538feb4859d3e93c9784965af206b2a0529ae59
s0nought <70777299+s0nought@users.noreply.github.com>
2023-05-13 21:49:26 +0300

    Initial commit

---
+++ modules/classes.py
@@ -0,0 +1,170 @@
+import random
+
+from modules.constants import (
+    SETTINGS_FILE_PATH
+)
+
+from modules.utils import (
+    load_json,
+    dump_json,
+    get_current_date
+)
+
+class Settings:
+    def __init__(self):
+        self.data = load_json(SETTINGS_FILE_PATH)
+        self.holidays = self.data["holidays"]
+        self.users = self.data["users"]
+        self.birthdays = dict()
+        self.dsm = self.data["dsm"]
+        self.release = self.data["release"]
+
+        self.generate_birthdays_map()
+
+    def save(self) -> None:
+        dump_json(self.data, SETTINGS_FILE_PATH)
+
+    # holidays
+
+    def is_holiday(self) -> bool:
+        return True if get_current_date() in self.holidays else False
+
+    def add_holiday(self, date: str) -> None:
+        if date not in self.holidays:
+            self.holidays.append(date)
+            self.save()
+
+    def remove_holiday(self, date: str) -> None:
+        if date in self.holidays:
+            self.holidays.remove(date)
+            self.save()
+
+    # users
+
+    def get_users(self) -> list[str]:
+        return list(self.users.keys())
+
+    def is_user(self, user_name: str) -> bool:
+        return True if user_name in self.get_users() else False
+
+    def get_first_name(self, user_name: str) -> str:
+        x = self.users.get(user_name)
+
+        return x["first_name"] if x else "-"
+
+    def update_user(self, user_name: str, first_name: str, birthday: str) -> None:
+        user_data = {
+            "first_name": first_name,
+            "birthday": birthday,
+            "is_on_vacation": False
+        }
+
+        self.users.update({user_name: user_data})
+        self.save()
+        self.generate_birthdays_map()
+
+    def remove_user(self, user_name: str) -> None:
+        if self.is_user(user_name):
+            self.users.pop(user_name)
+            self.save()
+            self.generate_birthdays_map()
+
+    # birthdays
+
+    def generate_birthdays_map(self) -> None:
+        self.birthdays.clear()
+
+        for user_name, user_data in self.users.items():
+            birthday_date = user_data.get("birthday")
+
+            x = self.birthdays.get(birthday_date)
+
+            if x == None:
+                self.birthdays.update({birthday_date: [user_name]})
+            else:
+                self.birthdays.update({birthday_date: x + [user_name]})
+
+    def get_birthday_boys(self) -> list[str]:
+        x = self.birthdays.get(get_current_date())
+
+        return x if x else []
+
+    # vacation
+
+    def get_vacation_list(self) -> list[str]:
+        result = list()
+
+        for user_name, user_data in self.users.items():
+            if user_data.get("is_on_vacation"):
+                result.append(user_name)
+
+        return result
+
+    def set_vacation_status(self, user_name: str, status: bool) -> None:
+        user_data = self.users.get(user_name)
+        user_data.update({"is_on_vacation": status})
+
+        self.users.update({user_name: user_data})
+        self.save()
+
+    # dsm
+
+    def get_dsm_hosts(self) -> list[str]:
+        return self.dsm.get("hosts")
+
+    def set_dsm_hosts(self, hosts: list[str]) -> None:
+        self.dsm.update({"hosts": hosts})
+        self.save()
+
+    def append_dsm_host(self, user_name: str) -> None:
+        self.set_dsm_hosts(self.get_dsm_hosts() + [user_name])
+
+    def get_dsm_current(self) -> str:
+        return self.dsm.get("current")
+
+    def set_dsm_current(self, user_name: str) -> None:
+        self.dsm.update({"current": user_name})
+        self.save()
+
+    def tick_dsm(self) -> None:
+        candidates = set(self.get_users())
+        candidates.difference_update(set(self.get_dsm_hosts())) # already hosted
+        candidates.difference_update(set(self.get_vacation_list())) # on vacation
+
+        candidates_list = list(candidates)
+
+        if len(candidates_list) == 0:
+            self.set_dsm_hosts([])
+            return self.tick_dsm()
+
+        host = random.choice(candidates_list)
+
+        self.append_dsm_host(host)
+        self.set_dsm_current(host)
+
+    # release
+
+    def get_release_order(self) -> list[str]:
+        return self.release.get("order")
+
+    def set_release_order(self, order: list[str]) -> None:
+        self.release.update({"order": order})
+        self.save()
+
+    def get_release_current(self) -> str:
+        return self.release.get("current")
+
+    def set_release_current(self, user_name: str) -> None:
+        self.release.update({"current": user_name})
+        self.save()
+
+    def tick_release(self):
+        order = self.get_release_order()
+        current_index = order.index(self.get_release_current())
+
+        next_index = current_index + 1 if (current_index + 1) < len(order) else 0
+        next_turn = order[next_index]
+
+        self.set_release_current(next_turn)
+
+settings = Settings()
---
+++ modules/utils.py
@@ -0,0 +1,36 @@
+import json
+from datetime import datetime, date, timezone
+
+from modules.constants import (
+    FIRST_PLANNING_DATETIME_UTC
+)
+
+def load_json(file_path: str, mode: str = "rt", encoding: str = "UTF-8"):
+    with open(file_path, mode = mode, encoding = encoding) as f:
+        return json.load(f)
+
+def dump_json(data, file_path: str, mode: str = "wt", encoding: str = "UTF-8", indent: int = 4) -> None:
+    with open(file_path, mode = mode, encoding = encoding) as f:
+        return json.dump(data, f, indent = indent)
+
+def get_datetime() -> datetime:
+    return datetime.now(timezone.utc)
+
+def format_datetime(dt: datetime, f: str = r"%d.%m.%Y %H:%M") -> str:
+    return dt.strftime(f)
+
+def get_current_date() -> str:
+    return format_datetime(get_datetime(), r"%d.%m")
+
+def get_weekday() -> int:
+    return date.isoweekday(get_datetime())
+
+def is_friday() -> bool:
+    return True if get_weekday() == 5 else False
+
+def is_weekend() -> bool:
+    return True if get_weekday() > 5 else False
+
+def is_planning_day() -> bool:
+    x = (get_datetime() - FIRST_PLANNING_DATETIME_UTC).days
+    return True if x % 14 == 0 else False
